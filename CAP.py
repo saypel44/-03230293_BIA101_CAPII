6ccec71c22ccff43a914b9f58b2345385bf01971
03230293 <03230293.gcbs@rub.edu.bt>
2024-04-20 10:59:20 +0600

    init

---
+++ CAP.py
@@ -0,0 +1,12 @@
+: 02908728_BIA101_CAPII
+
+class Taxpayer:
+  def __init__(self, income, employment_type, sector, children, nppf, gis):
+    self.income = income
+    self.employment_type = employment_type
+    self.sector = sector
+    self.children = children
+    self.nppf = nppf
+    self.gis = gis
+
+  # ... other methods for deductions, taxable income, and tax calculation
