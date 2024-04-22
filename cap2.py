e85e7c7be321338732b9e62125833e1a09ec11b1
03230293 <03230293.gcbs@rub.edu.bt>
2024-04-22 15:54:29 +0600

    TRied

--- cap2.py
+++ cap2.py
@@ -18,12 +18,6 @@ Detail= Taxpayer(basic_pay=14675, employment_type= "Regular", sector= "governmen
 
 class Income:
     def __init__(self, basic_pay_allowances, fees_remuneration, bonus, commission, leave_encashment, shareofProfirreceived, consultancy_income, benefitsReceived):
-      self.basic_pay = basic_pay
-      self.employment_type = employment_type
-      self.sector = sector
-      self.children = children
-      self.nppf = nppf
-      self.gis = gis
       self.basic_pay_allowances= basic_pay_allowances
       self.fees_remuneration=fees_remuneration
       self.bonus=bonus
